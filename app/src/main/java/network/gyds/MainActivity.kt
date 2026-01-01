a04e2a277c833df9216ff7949ea71613f46c717a
hc172808 <hc172808@gmail.com>
2026-01-01 13:47:31 -0400

    Update MainActivity.kt

--- app/src/main/java/network/gyds/MainActivity.kt
+++ app/src/main/java/network/gyds/MainActivity.kt
@@ -1,1 +1,15 @@
 
+package network.gyds
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        NodeService.start(this)
+    }
+}
