20d6768a8d1fa6c3c90e2d607dccb53f3598e29a
hc172808 <hc172808@gmail.com>
2026-01-01 13:49:08 -0400

    Update RpcClient.kt

--- app/src/main/java/network/gyds/RpcClient.kt
+++ app/src/main/java/network/gyds/RpcClient.kt
@@ -1,1 +1,15 @@
 
+package network.gyds
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object RpcClient {
+    private const val RPC_URL = "https://rpc.gyds.network"
+
+    fun connect() {
+        val client = OkHttpClient()
+        val req = Request.Builder().url(RPC_URL).build()
+        client.newCall(req).execute()
+    }
+}
