b247f1ada8a237fdf1afc7057dcd35665e28d65a
hc172808 <hc172808@gmail.com>
2026-01-01 13:49:27 -0400

    Update MinerWS.kt

--- app/src/main/java/network/gyds/MinerWS.kt
+++ app/src/main/java/network/gyds/MinerWS.kt
@@ -1,1 +1,19 @@
 
+package network.gyds
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+
+object MinerWS {
+
+    fun start() {
+        val client = OkHttpClient()
+        val req = Request.Builder()
+            .url("wss://rpc.gyds.network:8546/mine")
+            .build()
+
+        client.newWebSocket(req, object : WebSocketListener() {})
+    }
+}
