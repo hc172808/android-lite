53a9df2fa6c71ce42f47b439f37f272a55a1ed7d
hc172808 <hc172808@gmail.com>
2026-01-01 13:47:49 -0400

    Update NodeService.kt

--- app/src/main/java/network/gyds/NodeService.kt
+++ app/src/main/java/network/gyds/NodeService.kt
@@ -1,1 +1,12 @@
+package network.gyds
+
+import android.content.Context
+import android.content.Intent
+
+object NodeService {
+    fun start(context: Context) {
+        val intent = Intent(context, LiteNodeService::class.java)
+        context.startForegroundService(intent)
+    }
+}
 
